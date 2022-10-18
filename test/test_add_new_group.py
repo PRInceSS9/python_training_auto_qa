b9afb2be923d44d8520ec0d2115d0bbf916eb9f7
PRInceSS99 <kuskova.m.v@gmail.com>
2022-10-18 18:53:16 +0300

    lesson5 for groups

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -12,9 +12,9 @@ def app(request):
 
 def test_add_group(app):
     app.open_main_page()
-    app.open_group_page()
+    app.group.open_page()
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test", header="group test", footer="test group"))
+    app.group.create(Group(name="test", header="group test", footer="test group"))
     app.session.logout()
 
 
