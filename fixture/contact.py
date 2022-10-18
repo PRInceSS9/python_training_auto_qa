7ab07ae90775807ae829b316cf981e9c2e877a30
PRInceSS99 <kuskova.m.v@gmail.com>
2022-10-18 19:19:49 +0300

    lesson5 for contacts

---
+++ fixture/contact.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.support.ui import Select
+
+class ContactHelper:
+
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_link_text("home page").click()
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,68 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
+from fixture.application import Application
 from model.contact import Contact
 
-class test_add_new_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_new_contact(self):
-        wd = self.open_main_page()
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="Василий", middlename="Анатольевич", lastname="Кусков", bday="9",
-                       bmonth="November", byear="1998"))
-
-    def create_contact(self, wd, contact):
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home page").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_main_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook")
-        return wd
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+def test_add_new_contact(app):
+    app.open_main_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Василий", middlename="Анатольевич", lastname="Кусков", bday="9",
+                               bmonth="November", byear="1998"))
 
     
-    # def is_element_present(self, how, what):
-    #     try: self.driver.find_element(by=how, value=what)
-    #     except NoSuchElementException as e: return False
-    #     return True
-    #
-    # def is_alert_present(self):
-    #     try: self.driver.switch_to_alert()
-    #     except NoAlertPresentException as e: return False
-    #     return True
-    
-
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
