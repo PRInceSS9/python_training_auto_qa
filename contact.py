2d76fce8036a823c6bfc5b8cc9716842b78d3ed6
PRInceSS99 <kuskova.m.v@gmail.com>
2022-10-18 17:14:24 +0300

    lesson3

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, bday, bmonth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
---
+++ test_add_new_contact.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import unittest
+from contact import Contact
+
+class test_add_new_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+    
+    def test_add_new_contact(self):
+        wd = self.open_main_page()
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Contact(firstname="Василий", middlename="Анатольевич", lastname="Кусков", bday="9",
+                       bmonth="November", byear="1998"))
+
+    def create_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_link_text("home page").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_main_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook")
+        return wd
+
+    
+    # def is_element_present(self, how, what):
+    #     try: self.driver.find_element(by=how, value=what)
+    #     except NoSuchElementException as e: return False
+    #     return True
+    #
+    # def is_alert_present(self):
+    #     try: self.driver.switch_to_alert()
+    #     except NoAlertPresentException as e: return False
+    #     return True
+    
+
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
