89b31a77c431fcf2421e1b2a31e4841a7b73e18d
PRInceSS99 <kuskova.m.v@gmail.com>
2022-10-22 22:22:05 +0300

    lesson 10

--- fixture/group.py
+++ fixture/group.py
@@ -9,8 +9,8 @@ class GroupHelper:
 
     def open_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/group.php")
-        return wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def fill_group_form(self, group):
         wd = self.app.wd
