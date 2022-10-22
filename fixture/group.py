872e79bb3057096f3d462a165ea4f07e7cd5eff8
PRInceSS99 <kuskova.m.v@gmail.com>
2022-10-22 16:56:13 +0300

    fix update\modify defs

--- fixture/group.py
+++ fixture/group.py
@@ -1,44 +1,7 @@
 
-
 class GroupHelper:
     def __init__(self, app):
         self.app = app
-
-    def create(self, group):
-        wd = self.app.wd
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
-        wd.find_element_by_name("submit").click()
-
-    def delete_first(self):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("delete").click()
-
-    def update_first(self,group):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
-
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -48,3 +11,44 @@ class GroupHelper:
         wd = self.app.wd
         wd.get("http://localhost/addressbook/group.php")
         return wd
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+    def create(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        wd.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
+        wd.find_element_by_name("submit").click()
+
+    def delete_first(self):
+        wd = self.app.wd
+        self.select_first_group()
+        wd.find_element_by_name("delete").click()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_date):
+        wd = self.app.wd
+        self.open_page()
+        self.select_first_group()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_date)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+
+
--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,10 +1,25 @@
 from model.group import Group
 
 
-def test_update_first_group(app):
+def test_modify_group_name(app):
     app.open_main_page()
     app.session.login(username="admin", password="secret")
     app.group.open_page()
-    app.group.update_first(Group(name="update name", header="update header", footer="update footer"))
-    app.group.return_to_groups_page()
+    app.group.modify_first_group(Group(name="New name group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.open_main_page()
+    app.session.login(username="admin", password="secret")
+    app.group.open_page()
+    app.group.modify_first_group(Group(header="New name header"))
+    app.session.logout()
+
+
+def test_modify_group_footer(app):
+    app.open_main_page()
+    app.session.login(username="admin", password="secret")
+    app.group.open_page()
+    app.group.modify_first_group(Group(footer="New name footer"))
     app.session.logout()
